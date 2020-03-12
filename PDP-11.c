7c4add300f853d8022ff9c8bfff254193ad020bf
GlebShabuldaev <61522917+GlebShabuldaev@users.noreply.github.com>
2020-03-12 13:36:14 +0300

    Upadeted tests and fixed load_file

--- PDP-11.c
+++ PDP-11.c
@@ -13,7 +13,7 @@ void b_write(Adress adr, byte b);
 byte b_read(Adress adr);
 void w_write(Adress adr, word w);
 word w_read(Adress adr);
-void load_file(int argc, char  * argv[]);
+void load_file();
 
 void test_mem() {
 	byte b0 = 0x0a;
@@ -21,18 +21,22 @@ void test_mem() {
 	byte bres = b_read(2);
 	assert(b0 == bres);
 	Adress a = 4;
-	byte b1 = 0x0b;
+	byte b1 = 0xcb;
 	b0 = 0x0a;
-	word w = 0x0b0a;
+	word w = 0xcb0a;
 	b_write(a, b0);
 	b_write(a+1, b1);
 	word wres = w_read(a);
 	assert(w == wres);
+	w = 0x0a0b;
 	w_write(a, w);
+	wres = w_read(a);
+	assert(w == wres);
 }
 
 int main(int argc, char * argv[]) {
 	test_mem();
+	load_file();
 	return 0; 
 }
 
@@ -44,24 +48,41 @@ byte b_read(Adress adr) {
 	return mem[adr];
 }
 
-void w_write(Adress adr, word w){
+void w_write(Adress adr, word w) {
 	mem[adr] = w;
 	mem[adr + 1] = w >> 8;
 }
 
-word w_read(Adress adr){
+word w_read(Adress adr) {
 	word w = ((word)mem[adr+1]) << 8;
 	w = w | mem[adr];
 	return w;
 }
 
+void load_file() {
+	unsigned int adr_1;
+	unsigned int N;
+	while(scanf("%x%x", &adr_1, &N) == 2) {
+		for(unsigned i = 0; i < N; i++) {
+			unsigned int k;
+			scanf("%x", &k);
+			b_write(adr_1 + i, k);
+			printf("%x\n", b_read(adr_1 + i));
+		}
+	}
+}
+
+
+/*
 void load_file(int argc, char  * argv[]) {
 	FILE * fin =  fopen(argv[1], "r");
 	int adr_1;
 	char N;
 	scanf("%d %hhx", &adr_1, &N);
-	for(int i = 0; i < N; i++) {
+	for(unsigned i = 0; i < N; i++) {
 		byte k;
+		scanf("%hhx", &k);
 		b_write(adr_1 + i, k);
 	}
 }
+*/
